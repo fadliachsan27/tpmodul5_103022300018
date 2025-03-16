b66d780455a86a289acdfb14790791b37f59f20f
fadliachsan27 <fadliachsan010@gmail.com>
2025-03-17 00:47:52 +0700

    Menambah class HaloGeneric dengan method  SapaUser

---
+++ tpmodul5_103022300018/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul5_103022300018
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HaloGeneric halo = new HaloGeneric();
+            halo.SapaUser("Muhammad Fadli Achsan Kurniawan");
+        }
+    }
+}
